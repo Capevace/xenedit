247efca2385d15247ed4d34cffa4d8d34bcfd945
Lukas von Mateffy <luke@mateffy.me>
2020-06-10 06:30:33 +0200

    Added deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+git push -f git@github.com:capevace/xenedit.git master:gh-pages
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+
+cd -
